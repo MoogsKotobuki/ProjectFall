b348afe99b90ee2072263f5bf95b4c48dae6108e
Matthew Larrobis <matthew.larrobis20@gmail.com>
2026-03-24 09:09:00 +0800

    Initial Animaiton And Asset

--- Class/Falling.gd
+++ Class/Falling.gd
@@ -5,14 +5,11 @@ var gravity
 @onready var fixer:Fixer = Fixer.new()
 
 var Speed:float
-@export var accel = 5
-@export var decel = 10
-@export var maxJump = 1
+@export var accel:float = 5.0
+@export var maxJump:float = 1.0
+@export var friction:float = 0.5
 var jumpLimit = 0
-
-var frictionLand = 2.0
-var friction = 1
-var globalDirection = 0
+var doublejump = false
 
 var globalInputStrenght = 0
 func enter() -> void:
@@ -21,6 +18,10 @@ func enter() -> void:
 	
 	Speed = Entity.previousMaxSpeed
 func _update(_delta: float) -> void:
+	if Entity.velocity.y < 5.0 and doublejump == true:
+			state_machine.change_state("jump")
+			doublejump = false
+	
 	var raw_input = Vector2(IHandler.mov.x,0)
 	var input_strenght = raw_input.length()
 	globalInputStrenght = input_strenght
@@ -28,29 +29,39 @@ func _update(_delta: float) -> void:
 		Speed = Entity.runSpeed
 	
 func _physics_update(_delta: float) -> void:
+	#Falling Logic
 	if !Entity.is_on_floor():
 		Entity.velocity.y += (Entity.get_gravity().y * Entity.mass) * _delta
 		midAirMovement(_delta)
+		
+		#Double Jump Logic
 		if Input.is_action_just_pressed("jump") and jumpLimit < maxJump:
 			jumpLimit+= 1
-			state_machine.change_state("jump")
+			doublejump = true
 			if globalInputStrenght == 0:
+				#cancelling velocity during double when movement controller is not touch
 				Entity.velocity.x = 0
 	else:
+		#Reset Double jump Counter
 		jumpLimit = 0
 		state_machine.change_state("idle")
 		if globalInputStrenght == 0:
-			Entity.velocity.x = Entity.velocity.x * 0.5
+			#Reducing Velocity when landing
+			Entity.velocity.x = Entity.velocity.x * friction
 	Entity.move_and_slide()
 
 func midAirMovement(_delta) -> void:
+	#same logic as running with different acceleration value
 	var direction = IHandler.mov.x
+	var globalDirection
+	#this is inefficient or maybe this is the solution
+	if direction > 0:
+		globalDirection =1
+	else:
+		globalDirection = -1
+		
+	
 	if direction:
-		if direction > 0:
-			globalDirection =1
-		else:
-			globalDirection = -1
-			
 		if Entity.velocity.x > 0:
 				Entity.viewSides = PI
 		elif Entity.velocity.x < 0:
--- Class/Jump.gd
+++ Class/Jump.gd
@@ -2,18 +2,16 @@ extends State
 var cVelocity
 
 var jumpLimit :int= 0
-@onready var timer = $Timer
 
 func enter() -> void:
 	var r = ReadyState.new()
 	r.Ready(self,state_machine,1)
-		
+	
 func _update(_delta: float) -> void:
 	pass
 func exit()-> void:
 	pass
 func _physics_update(_delta: float) -> void:
 	Entity.velocity.y = Entity.JumpForce * 10
-	if timer.is_stopped():
-		state_machine.change_state("fall")
+	state_machine.change_state("fall")
 	Entity.move_and_slide()
