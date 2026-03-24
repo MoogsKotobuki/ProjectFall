6bc56450f9490ccbc904157c79ac56caef4386ee
Matthew Larrobis <matthew.larrobis20@gmail.com>
2026-03-24 13:02:53 +0800

    animation upadte #1

--- Class/Falling.gd
+++ Class/Falling.gd
@@ -13,13 +13,16 @@ var jumpLimit = 0
 var frictionLand = 2.0
 var friction = 1
 var globalDirection = 0
+var doubleJump = false
 
 var globalInputStrenght = 0
 func enter() -> void:
 	var r = ReadyState.new()
 	r.Ready(self,state_machine,1)
 	
+	jumpLimit += 1
 	Speed = Entity.previousMaxSpeed
+
 func _update(_delta: float) -> void:
 	var raw_input = Vector2(IHandler.mov.x,0)
 	var input_strenght = raw_input.length()
@@ -27,13 +30,17 @@ func _update(_delta: float) -> void:
 	if input_strenght >0.75:
 		Speed = Entity.runSpeed
 	
+	if Entity.velocity.y < 5.0 and doubleJump == true:
+		state_machine.change_state("jump")
+		doubleJump = false
+	
 func _physics_update(_delta: float) -> void:
 	if !Entity.is_on_floor():
 		Entity.velocity.y += (Entity.get_gravity().y * Entity.mass) * _delta
 		midAirMovement(_delta)
 		if Input.is_action_just_pressed("jump") and jumpLimit < maxJump:
 			jumpLimit+= 1
-			state_machine.change_state("jump")
+			doubleJump = true
 			if globalInputStrenght == 0:
 				Entity.velocity.x = 0
 	else:
